c8238d6258c579f84502c744633acc6e6c96e0dc
Riccardo Novaglia <riccardo.novaglia@gmail.com>
2021-01-11 08:32:45 +0000

    Organise karabiner, update brew command

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -6,11 +6,11 @@ alias ia="npm run install:all"
 alias i="npm install"
 alias run="npm run"
 alias r="run"
-alias cy="cypress"
-alias story="npm run storybook"
 
 alias zshrc="vi ~/.zshrc && source ~/.zshrc"
 alias z="zshrc"
 alias idea='IDEA=$(ls -1d /Applications/IntelliJ\ * | tail -n1); open -a "$IDEA" "$1"'
 # multi-user brew fix
-alias fix_brew='sudo chown -R $(whoami) /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew/locks && chmod u+w /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew/locks'
+alias fix_brew='\
+sudo chown -R $(whoami) /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew/locks /usr/local/share/zsh/site-functions/_brew_services /usr/local/Homebrew/Library/Taps/homebrew/homebrew-services/completions/zsh/_brew_services && \
+sudo chmod u+w          /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew/locks /usr/local/share/zsh/site-functions/_brew_services /usr/local/Homebrew/Library/Taps/homebrew/homebrew-services/completions/zsh/_brew_services'
