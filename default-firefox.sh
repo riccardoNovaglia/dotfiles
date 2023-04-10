468b6f4c06fcdca5c661d7ab273769fe20b633c8
riccardoNovaglia <riccardo.novaglia@gmail.com>
2023-04-10 11:16:22 +0100

    Script to default browser to firefox

---
+++ default-firefox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Default Firefox
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🦊
+
+defaultbrowser firefoxdeveloperedition
