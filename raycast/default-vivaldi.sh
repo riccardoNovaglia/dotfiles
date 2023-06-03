4db5ac77123a714d9ece4fd2605482f703caf563
riccardoNovaglia <riccardo.novaglia@gmail.com>
2023-06-03 10:52:15 +0200

    Organise raycast scripts

---
+++ raycast/default-vivaldi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Default Vivaldi
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🎻
+
+# Documentation:
+# @raycast.description Set default browser to Vivaldi
+
+defaultbrowser vivaldi
