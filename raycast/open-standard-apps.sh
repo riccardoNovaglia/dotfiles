19826a2097cf2c838535ca71a23b34d35b624318
riccardoNovaglia <riccardo.novaglia@gmail.com>
2024-04-28 15:51:34 +0100

    Raycast script for opening standard apps

---
+++ raycast/open-standard-apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open standard apps
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+open -a "Google Chrome"
+open -a "Slack"
+/Users/riccardo.novaglia/.idea/cmd/pycharm1 ~/projects/kraken-core
