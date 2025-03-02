7149ef29b180e3f31477d6f97b79d4aa1e935ea2
riccardoNovaglia <riccardo.novaglia+github@gmail.com>
2025-03-02 14:30:06 +0000

    Add a few aliases to deal with aliases

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -1,3 +1,10 @@
 source ~/projects/me/dotfiles/zsh/aliases/common.zsh
 source ~/projects/me/dotfiles/zsh/aliases/git.zsh
 source ~/projects/me/dotfiles/zsh/aliases/kraken.zsh
+
+
+alias aliases="cat ~/projects/me/dotfiles/zsh/aliases/* | grep \"alias \" | fzf"
+alias aliases_search="aliases"
+alias aliases_edit="fleet ~/projects/me/dotfiles/zsh/aliases"
+alias aliases_update="source ~/projects/me/dotfiles/zsh/aliases/aliases.zsh"
+
