8909b40c0e634dfebe1210c2ee07cf3dc12f5144
riccardoNovaglia <riccardo.novaglia+github@gmail.com>
2025-04-23 15:38:51 +0100

    Update some env vars for Kraken

--- zsh/kraken.zsh
+++ zsh/kraken.zsh
@@ -1,3 +1,12 @@
 # Disable DD locally
 export DD_DOGSTATSD_DISABLE=1;
+export DD_INSTRUMENTATION_TELEMETRY_ENABLED=0
+
+#
 export KRAKEN_CLIENT='oegb';
+
+# Python lazy loading, useful locally
+export PY_LAZY_LOADING=1
+
+# Cloudfarer
+export KRAKEN_CLI_USE_FZF=1
