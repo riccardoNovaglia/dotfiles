d94accf5a30d21230e1ec2bf352186926d76ff83
riccardoNovaglia <riccardo.novaglia+github@gmail.com>
2025-04-30 09:18:45 +0100

    Update aliases to be easier to type

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -4,6 +4,7 @@ source ~/projects/me/dotfiles/zsh/aliases/kraken.zsh
 source ~/projects/me/dotfiles/zsh/aliases/js.zsh
 
 alias aliases="cat ~/projects/me/dotfiles/zsh/aliases/* | grep \"alias \" | fzf"
-alias aliases_search="aliases"
-alias aliases_edit="fleet ~/projects/me/dotfiles/zsh/aliases"
-alias aliases_update="source ~/projects/me/dotfiles/zsh/aliases/aliases.zsh"
+alias aliases-search="aliases"
+alias aliases-edit="fleet ~/projects/me/dotfiles/zsh/aliases"
+alias aliases-update="source ~/projects/me/dotfiles/zsh/aliases/aliases.zsh"
+alias aliases-update="source ~/projects/me/dotfiles/zsh/aliases/aliases.zsh"
